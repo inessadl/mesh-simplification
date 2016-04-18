bafcf8b2596a198cd8429918f830720412bd2509
Inessa Luerce <idluerce@inf.ufpel.edu.br>
2016-04-18 15:56:06 -0300

    assignment1 project deleted

--- Assignment-01/CG_UFPel/include/controls.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef CONTROLS_HPP
-#define CONTROLS_HPP
-
-void computeMatricesFromInputs(int nUseMouse = 0, int nWidth = 1024, int nHeight = 768);
-glm::mat4 getViewMatrix();
-glm::mat4 getProjectionMatrix();
-
-#endif
