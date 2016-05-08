0662b4961665bdd07e307ea7b450277efc8d1f8b
Inessa Luerce <idluerce@inf.ufpel.edu.br>
2016-05-08 15:24:45 -0300

    camera class added

---
+++ Assignment03/include/camera.hpp
@@ -0,0 +1,17 @@
+//
+// Created by inessa on 08/05/16.
+//
+
+#ifndef ASSIGNMENT03_CAMERA_HPP
+#define ASSIGNMENT03_CAMERA_HPP
+
+
+class Camera
+{
+
+    Camera();
+    ~Camera();
+};
+
+
+#endif //ASSIGNMENT03_CAMERA_HPP
---
+++ Assignment03/sources/camera.cpp
@@ -0,0 +1,17 @@
+//
+// Created by inessa on 08/05/16.
+//
+
+#include "camera.hpp"
+
+/* Constructor */
+Camera::Camera()
+{
+
+}
+
+/* Destructor */
+Camera::~Camera()
+{
+
+}
