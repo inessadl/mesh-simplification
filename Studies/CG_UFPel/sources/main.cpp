a34de44fbe4f3ba2783e22e54c17b85d5f1b74dc
Inessa Luerce <idluerce@inf.ufpel.edu.br>
2016-06-22 11:00:54 -0300

    initial transformations and tests with keyboard input

--- Studies/CG_UFPel/sources/main.cpp
+++ Studies/CG_UFPel/sources/main.cpp
@@ -95,8 +95,8 @@ int main(void)
 	glfwSetInputMode(g_pWindow, GLFW_STICKY_KEYS, GL_TRUE);
 	glfwSetCursorPos(g_pWindow, g_nWidth / 2, g_nHeight / 2);
 
-	// Dark blue background
-	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+	// Dark grey background
+	glClearColor(0.2f, 0.2f, 0.2f, 0.0f);
 
 	// Enable depth test
 	glEnable(GL_DEPTH_TEST);
@@ -114,16 +114,25 @@ int main(void)
 
 	// TODO set the correct shader
 //	GLuint programID = LoadShaders("shaders/StandardShading.vertexshader", "shaders/StandardShading.fragmentshader");
-	GLuint programID = LoadShaders("shaders/Test.vertexshader", "shaders/Test.fragmentshader");
+//	GLuint programID = LoadShaders("shaders/ShadowMapping.vertexshader", "shaders/ShadowMapping.fragmentshader");
+//	GLuint programID = LoadShaders("shaders/StandardShadingRTT.vertexshader", "shaders/StandardShadingRTT.fragmentshader");
+	GLuint programID = LoadShaders("shaders/ShadowMapping_SimpleVersion.vertexshader", "shaders/ShadowMapping_SimpleVersion.fragmentshader");
+//  GLuint programID = LoadShaders("shaders/NormalMapping.vertexshader", "shaders/NormalMapping.fragmentshader");
+//  GLuint programID = LoadShaders("shaders/Passthrough.vertexshader", "shaders/WobblyTexture.fragmentshader");
 
 
-	// Get a handle for our "MVP" uniform
+
+
+    // Get a handle for our "MVP" uniform
 	GLuint MatrixID      = glGetUniformLocation(programID, "MVP");
 	GLuint ViewMatrixID  = glGetUniformLocation(programID, "V");
 	GLuint ModelMatrixID = glGetUniformLocation(programID, "M");
 
 	// Load the texture
-	GLuint Texture = loadDDS("mesh/goose.dds");
+	GLuint Texture = loadDDS("mesh/uvmap.DDS");
+//    GLuint Texture = loadDDS("mesh/ExampleBillboard.DDS");
+//    GLuint Texture = loadDDS("mesh/Fire.dds");
+
 
 	// Get a handle for our "myTextureSampler" uniform
 	GLuint TextureID = glGetUniformLocation(programID, "myTextureSampler");
@@ -133,8 +142,12 @@ int main(void)
 	std::vector<glm::vec2> uvs;
 	std::vector<glm::vec3> normals;
 	bool res = loadOBJ("mesh/suzanne.obj", vertices, uvs, normals);
+//	bool res = loadOBJ("mesh/wheel.obj", vertices, uvs, normals);
+//    bool res = loadOBJ("mesh/cube.obj", vertices, uvs, normals);
+    bool second_object = loadOBJ("mesh/goose.obj", vertices, uvs, normals);
 
-	std::vector<unsigned short> indices;
+
+    std::vector<unsigned short> indices;
 	std::vector<glm::vec3> indexed_vertices;
 	std::vector<glm::vec2> indexed_uvs;
 	std::vector<glm::vec3> indexed_normals;
@@ -201,7 +214,99 @@ int main(void)
 		glm::mat4 ProjectionMatrix = getProjectionMatrix();
 		glm::mat4 ViewMatrix       = getViewMatrix();
 		glm::mat4 ModelMatrix      = glm::mat4(1.0);
+
+//		translação que move o macaco e o ganso
+//		ModelMatrix = translate(ModelMatrix, glm::vec3(-3, 0, -7));
 		glm::mat4 MVP              = ProjectionMatrix * ViewMatrix * ModelMatrix;
+
+
+		// position
+		glm::vec3 position = glm::vec3( 0, 0, 5 );
+		// horizontal angle : toward -Z
+		float horizontalAngle = 3.14f;
+		// vertical angle : 0, look at the horizon
+		float verticalAngle = 0.0f;
+		// Initial Field of View
+		float initialFoV = 45.0f;
+
+		float speed = 3.0f; // 3 units / second
+		float mouseSpeed = 0.005f;
+
+		// Get mouse position
+		int xpos, ypos;
+		//glfwGetMousePos(&xpos, &ypos);
+
+		// Reset mouse position for next frame
+//		glfwSetMousePos(1024/2, 768/2);
+
+		double CurrentTime = glfwGetTime();
+		float deltaTime = float(currentTime - lastTime);
+
+
+		// Compute new orientation
+		horizontalAngle += mouseSpeed * deltaTime * float(1024/2 - xpos );
+		verticalAngle   += mouseSpeed * deltaTime * float( 768/2 - ypos );
+
+
+		// Compute new orientation
+//		horizontalAngle += mouseSpeed *  float(1024/2 - xpos );
+//		verticalAngle   += mouseSpeed * float( 768/2 - ypos );
+
+
+		// Direction : Spherical coordinates to Cartesian coordinates conversion
+		glm::vec3 direction(
+				cos(verticalAngle) * sin(horizontalAngle),
+				sin(verticalAngle),
+				cos(verticalAngle) * cos(horizontalAngle)
+		);
+
+
+		// Right vector
+		glm::vec3 right = glm::vec3(
+				sin(horizontalAngle - 3.14f/2.0f),
+				0,
+				cos(horizontalAngle - 3.14f/2.0f)
+		);
+
+
+		// Up vector : perpendicular to both direction and right
+		glm::vec3 up = glm::cross( right, direction );
+
+		// Move forward
+		if (glfwGetKey(g_pWindow, GLFW_KEY_UP ) == GLFW_PRESS){
+			position += direction * deltaTime * speed;
+		}
+// Move backward
+		if (glfwGetKey(g_pWindow, GLFW_KEY_DOWN ) == GLFW_PRESS){
+			position -= direction * deltaTime * speed;
+		}
+// Strafe right
+		if (glfwGetKey(g_pWindow, GLFW_KEY_RIGHT ) == GLFW_PRESS){
+			position += right * deltaTime * speed;
+		}
+// Strafe left
+		if (glfwGetKey(g_pWindow, GLFW_KEY_LEFT ) == GLFW_PRESS){
+			position -= right * deltaTime * speed;
+		}
+
+
+		// Projection matrix : 45&deg; Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
+		ProjectionMatrix = glm::perspective(initialFoV, 4.0f / 3.0f, 0.1f, 100.0f);
+// Camera matrix
+		ViewMatrix       = glm::lookAt(
+				position,           // Camera is here
+				position+direction, // and looks here : at the same position, plus "direction"
+				up                  // Head is up (set to 0,-1,0 to look upside-down)
+		);
+
+
+		// Cull triangles which normal is not towards the camera
+		glEnable(GL_CULL_FACE);
+
+
+
+
+
 
 		// Send our transformation to the currently bound shader,
 		// in the "MVP" uniform
